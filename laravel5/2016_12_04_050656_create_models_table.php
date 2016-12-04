352cafe24c4e6780850be78933bcfde8d77fd184
Josué Artaud <jartaud@users.noreply.github.com>
2016-12-04 02:01:48 -0500

    Models migration

---
+++ laravel5/2016_12_04_050656_create_models_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateModelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('models', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('make_id');
+            $table->string('code', 125);
+            $table->string('title', 125);
+            $table->foreign('make_id')->references('id')->on('makes')->onDelete('CASCADE');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('models');
+    }
+}
